aee6ba425da9190cc19b68545d009f8c9a77bf84
EliasEresh <lasrifielias@gmail.com>
2023-04-27 09:21:07 +0200

    Router installed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import data from './logements.json';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   console.log(data);
   return (
-    <div>
-      <h1>Liste</h1>
-      {data.map((logement) => (
-        <div key={logement.id}>
-          <h2>{logement.title}</h2>
-          <p>{logement.description}</p>
-        </div>
-      ))}
-    </div>
+    <Router>
+      <div>
+        <h1>Liste</h1>
+        {data.map((logement) => (
+          <div key={logement.id}>
+            <h2>{logement.title}</h2>
+            <p>{logement.description}</p>
+          </div>
+        ))}
+      </div>
+    </Router>
   );
 }
 
