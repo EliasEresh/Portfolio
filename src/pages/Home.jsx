3e1eb8e48e23171366147ee26f344d6b0bf2c21c
EliasEresh <lasrifielias@gmail.com>
2023-08-17 20:19:07 +0200

    Navigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,16 @@ import '../style/index.css';
 import Gallery from '../components/projects/gallery.jsx';
 import About from '../components/About/about.jsx';
 import Skills from '../components/Skills/skills.jsx';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        // Use the navigate function to navigate to a different route
+        navigate('/some-route');
+    };
+
     return (
         <div>
             <div className="intro">
@@ -12,6 +20,7 @@ function Home() {
                 <p className="intro-paragraph">
                     Juriste reconverti dans les métiers du numérique, je m’attelle désormais à développer et designer des sites, apte au freelance et au travail en entreprise.
                 </p>
+                <button onClick={handleButtonClick}>Go to Another Page</button>
             </div>
             <Gallery />
             <Skills />
@@ -20,4 +29,4 @@ function Home() {
     )
 }
 
-export default Home
+export default Home;
